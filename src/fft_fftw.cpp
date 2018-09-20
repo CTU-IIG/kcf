f621ccc106b29823aef148162241929d6ca3d7ad
Shanigen <vkaraf@gmail.com>
2018-09-20 14:01:50 +0200

    Values are not wrriten to variables of child classes in FFT::init.

--- src/fft_fftw.cpp
+++ src/fft_fftw.cpp
@@ -18,6 +18,15 @@ void Fftw::init(unsigned width, unsigned height, unsigned num_of_feats, unsigned
 {
     Fft::init(width, height, num_of_feats, num_of_scales);
 
+    m_width = width;
+    m_height = height;
+    m_num_of_feats = num_of_feats;
+#ifdef BIG_BATCH
+    m_num_of_scales = num_of_scales;
+#else
+    (void)num_of_scales;
+#endif
+
 #if (!defined(ASYNC) && !defined(CUFFTW)) && defined(OPENMP)
     fftw_init_threads();
 #endif // OPENMP
