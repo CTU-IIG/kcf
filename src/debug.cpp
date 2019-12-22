71b7a1c122353214f00cbc1bb2a72fa56df1e1d5
Your Name <you@example.com>
2019-12-22 18:53:22 +0100

    Opravena drobná chyba ve výpisu debug. - pro výpis cv::Mat je teď zohledněna přítomnost váce kanálů, při volbě počtu vypsaných elementů

--- src/debug.cpp
+++ src/debug.cpp
@@ -8,7 +8,7 @@ std::ostream &operator<<(std::ostream &os, const DbgTracer::Printer<cv::Mat> &p)
     os << p.obj.size << " " << p.obj.channels() << "ch ";// << static_cast<const void *>(p.obj.data);
     os << " = [ ";
     const size_t num = 10; //p.obj.total();
-    for (size_t i = 0; i < std::min(num, p.obj.total()); ++i)
+    for (size_t i = 0; i < std::min(num, p.obj.total() * p.obj.channels()); ++i)
         os << p.obj.ptr<float>()[i] << ", ";
     os << (num < p.obj.total() ? "... ]" : "]");
     return os;
