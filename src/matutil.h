9f1d1592cc5219ecc8d407c71105b18f40f48879
Your Name <you@example.com>
2019-12-29 17:55:57 +0100

    plane3() prejmenovana na plane() v MatUtil

--- src/matutil.h
+++ src/matutil.h
@@ -25,7 +25,7 @@ static cv::Mat plane(uint scale, uint feature, cv::Mat &host) {
  * 
  * This will probably replace MatUtil::plane()
  **/
-static cv::Mat plane3(uint dim0, cv::Mat &host) {
+static cv::Mat plane(uint dim0, cv::Mat &host) {
     assert(host.dims == 3);
     assert(int(dim0) < host.size[0]);
     return cv::Mat(host.size[1], host.size[2], host.type(), host.ptr(dim0));
